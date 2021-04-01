5c9a74d2a48f606a31ce414c492603f365c43737
Michael Andre Hauser <michael.hauser27@gmail.com>
2021-04-01 16:39:36 +0200

    history readout/notify

--- ble/bleclient/src/main/java/at/qe/skeleton/bleclient/Main.java
+++ ble/bleclient/src/main/java/at/qe/skeleton/bleclient/Main.java
@@ -1,15 +1,37 @@
 package at.qe.skeleton.bleclient;
 
+import com.google.common.io.ByteSource;
 import com.google.common.io.ByteStreams;
 import org.apache.commons.io.IOUtils;
+import org.sputnikdev.bluetooth.manager.GattCharacteristic;
 import tinyb.*;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+
+class ValueNotification implements BluetoothNotification<byte[]> {
+    public void run(byte[] tempRaw) {
+            System.out.print(tempRaw[0]);
+
+        /*
+         * The temperature service returns the data in an encoded format which can be found in the wiki. Convert the
+         * raw temperature format to celsius and print it. Conversion for object temperature depends on ambient
+         * according to wiki, but assume result is good enough for our purposes without conversion.
+         */
+
+    }
+}
+
+class ConnectedNotification implements BluetoothNotification<Boolean> {
+    public void run(Boolean connected) {
+        System.out.println("Connected");
+    }
+}
 
 
 // TODO: use logging instead of System.out/System.err
@@ -20,6 +42,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public final class Main {
 
     static boolean running = true;
+    static boolean notify = false;
 
     private Main() {    }
 
@@ -105,14 +128,53 @@ public final class Main {
                 createFile(device.getAddress());
                 // TODO: read from device
                 System.out.println("Try to read out data from: " + device.getName());
-                Lock lock = new ReentrantLock();
-                lock.lock();
                 readDataFromTimeflip(device);
-                lock.unlock();
+   //             device.enableConnectedNotifications(new ConnectedNotification());
+   //             notifyFacet(device);
                 device.disconnect();
             } else {
                 System.out.println("Connection not established - trying next one");
             }
+        }
+    }
+
+    private static void notifyFacet(BluetoothDevice device) throws InterruptedException {
+        Lock lock = new ReentrantLock();
+        Condition cv = lock.newCondition();
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                running = false;
+                lock.lock();
+                try {
+                    cv.signalAll();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        });
+        BluetoothGattService timeflipService = getService(device, "f1196f50-71a4-11e6-bdf4-0800200c9a66");
+        if(timeflipService == null ) {
+            System.err.println("timeflip service is null");
+            device.disconnect();
+            System.exit(1);
+        }
+        setTimeflipPasswd(device, timeflipService);
+        boolean set = timeflipStatus(device, timeflipService);
+        set = notifyFacet(device, timeflipService);
+        BluetoothGattCharacteristic facetChar = getCharacteristic(timeflipService, "f1196f52-71a4-11e6-bdf4-0800200c9a66");
+        if(facetChar == null) {
+            System.err.println("facetChar is Null!");
+            device.disconnect();
+            System.exit(1);
+        }
+        facetChar.enableValueNotifications(new ValueNotification());
+        lock.lock();
+        try {
+            while(running)
+                cv.await();
+        } finally {
+            lock.unlock();
         }
     }
 
@@ -133,17 +195,21 @@ public final class Main {
         //set password for current session, else we cannot read write
         setTimeflipPasswd(device, timeflipService);
 
-        boolean set = timeflipStatus(device, timeflipService);
-        System.out.println("status: " + getTimeflipCommandResultOutput(device, timeflipService));
-        set = lockTimeflip(device, timeflipService);
-        System.out.println("current facet: " + getCurrentTimeflipFacet(device, timeflipService));
+        boolean set;
+        set = deleteHistory(device, timeflipService);
+        set = deleteHistory(device, timeflipService);
+        set = deleteHistory(device, timeflipService);
+        set = deleteHistory(device, timeflipService);
+        set = deleteHistory(device, timeflipService);
+        set = deleteHistory(device, timeflipService);
+        set = deleteHistory(device, timeflipService);
+        set = deleteHistory(device, timeflipService);
+        System.out.println("sleep 20 seconds, history is clear");
+        Thread.sleep(180000);
         set = readHistory(device, timeflipService);
-        System.out.println("current history: " + getTimeflipCommandResultOutput(device, timeflipService));
-        set = timeflipStatus(device, timeflipService);
-        System.out.println("status: " + getTimeflipCommandResultOutput(device, timeflipService));
-        set = unlockTimeflip(device, timeflipService);
-        set = timeflipStatus(device, timeflipService);
-        System.out.println("status: " + getTimeflipCommandResultOutput(device, timeflipService));
+        System.out.println("history" + getTimeflipCommandResultOutputHistory(device, timeflipService));
+
+        System.out.println(getCurrentTimeflipFacet(device, timeflipService));
     }
 
     private static void createFile(String address) {
@@ -197,6 +263,7 @@ public final class Main {
         byte[] facetRaw = facetChar.readValue();
         return facetRaw[0];
     }
+
     /** List of possible commands
      *  0x01 history read out request
      *  0x02 delete history
@@ -207,8 +274,6 @@ public final class Main {
      *  or on successful password write.
      *  0x06 0x01 - pause*** function on 0x06 0x02 – pause function off
      * */
-
-
     private static boolean setTimeflipCommand(BluetoothDevice device, BluetoothGattService timeflipService, byte[] command) throws InterruptedException {
         BluetoothGattCharacteristic commandChar = getCharacteristic(timeflipService, "f1196f54-71a4-11e6-bdf4-0800200c9a66");
         if(commandChar == null) {
@@ -230,6 +295,20 @@ public final class Main {
 
         byte[] history = commandResultChar.readValue();
         return Arrays.toString(history);
+    }
+
+    private static String getTimeflipCommandResultOutputHistory(BluetoothDevice device, BluetoothGattService timeflipService) throws InterruptedException {
+        BluetoothGattCharacteristic commandResultChar = getCharacteristic(timeflipService, "f1196f53-71a4-11e6-bdf4-0800200c9a66");
+        if(commandResultChar == null) {
+            System.err.println("commandResultChar is Null!");
+            device.disconnect();
+            System.exit(1);
+        }
+        readAllHistory(commandResultChar);
+//        byte[] history = commandResultChar.readValue();
+//        parser(history);
+//        return Arrays.toString(history);
+        return "abc";
     }
 
     private static boolean timeflipStatus(BluetoothDevice device, BluetoothGattService timeflipService) throws InterruptedException {
@@ -278,6 +357,71 @@ public final class Main {
         if(setCommand)
             return true;
         return false;
+    }
+
+    private static boolean notifyFacet(BluetoothDevice device, BluetoothGattService timeflipService) throws InterruptedException {
+        byte[] command = { 0x10 };
+        boolean setCommand = setTimeflipCommand(device, timeflipService, command);
+        if(setCommand)
+            return true;
+        return false;
+    }
+
+    private static List<String> readAllHistory(BluetoothGattCharacteristic chara) {
+        byte[] history = chara.readValue();
+        List<String> all = new ArrayList<>();
+        List<Byte> byteList = new ArrayList<>();
+        while(true) {
+            boolean wh = true;
+            for (int i = 0; i < history.length; i++) {
+                String byteStr = Byte.toString(history[i]);
+                if ( !(byteStr.equals("0")) ) {
+                    wh = true;
+                    break;
+                }
+                else
+                    wh = false;
+            }
+            if(!wh)
+                break;
+            for (int i = 0; i < history.length; i++) {
+                all.add(Byte.toString(history[i]));
+                byteList.add(history[i]);
+            }
+            history = chara.readValue();
+        }
+        parseHistory(all, byteList);
+        return all;
+    }
+
+    /**
+     * 2^18 mögliche zeit elemente welche in 3 bytes aufgeteilt werden
+     * 2^18 = 2^8*2^8*2^2
+     * für uns ist hierbei aber nicht der ganze suchraum essentiel
+     * da wir höchstens elemente mit 3 minuten stoppen wollen
+     * daher konzentrieren wir uns nur auf die ersten zwei bytes
+     * auf xx sowie yy
+     *
+     */
+
+    private static void parseHistory(List<String> historyList, List<Byte> byteList) {
+        for (int i = 0; i < historyList.size(); i=i+3) {
+            if(historyList.get(i+2).equals("0"))
+                continue;
+            int time = 0;
+            for (int j = 0; j < 3; j++) {
+                if(j == 2) {
+                    //oder einen bit shift (byte >> 2) vornehmen da nur 6 bit unsere facetid repräsentieren
+                    int facetId = (Integer.parseInt(historyList.get(i + j))) / 4;
+                    //2er complement auslesen und zusammenfuehren, von hex nach in umwandeln
+                    //https://manderc.com/concepts/umrechner/index.php
+                    String xx = String.format(("%x"), byteList.get(i));
+                    String yy = String.format(("%x"), byteList.get(i+1));
+                    String res = yy.concat(xx);
+                    System.out.println("facet:  " + facetId + " time: " + Integer.parseInt(res, 16));
+                }
+            }
+        }
     }
 
 }
