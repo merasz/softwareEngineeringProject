bd9bd8d953a950e13e2bbf39627f4a6e5d843679
Michael Andre Hauser <michael.hauser27@gmail.com>
2021-04-24 14:58:48 +0200

    termcreation fixed

--- src/main/java/at/qe/skeleton/ui/controllers/TermsController.java
+++ src/main/java/at/qe/skeleton/ui/controllers/TermsController.java
@@ -26,7 +26,6 @@ public class TermsController extends Controller implements Serializable {
 
     private List<Topic> topics;
     private List<Term> terms;
-    private Topic topic;
     private Term term;
 
     @PostConstruct
@@ -40,17 +39,6 @@ public class TermsController extends Controller implements Serializable {
         term = new Term();
     }
 
-    public Topic getTopic() {
-        return topic;
-    }
-
-    public void setTopic(Topic topic) {
-        System.out.println("current topic: " + topic.getTopicName());
-        this.topic = topic;
-        this.term.setTopic(topic);
-        System.out.println("current topic from term : " + this.term.getTopic().getTopicName());
-    }
-
     public Term getTerm() {
         return term;
     }
@@ -60,7 +48,6 @@ public class TermsController extends Controller implements Serializable {
     }
 
     public void doSaveTerm(Topic topic) {
-        System.out.println("doSave Terms");
         if(topic == null)
             System.out.println("topic null");
         else
@@ -82,17 +69,8 @@ public class TermsController extends Controller implements Serializable {
 
 
     public List<Term> getTermsOfTopic() {
-        terms = termsService.getTermsRepository().findAllByTopic(this.topic);
+        terms = termsService.getTermsRepository().findAllByTopic(term.getTopic());
         return terms;
-    }
-
-    public void addTopic(String name) {
-        try {
-            termsService.saveTopic(name, new Topic());
-            displayInfo("Topic created", "New topic successfully created.");
-        } catch (IllegalArgumentException e) {
-            displayError("Topic not created", e.getMessage());
-        }
     }
 
 //    public void addTerm(String name) {
@@ -104,15 +82,6 @@ public class TermsController extends Controller implements Serializable {
 //        }
 //    }
 
-    public void editTopic(String name) {
-        try {
-            termsService.saveTopic(name, topic);
-            displayInfo("Topic updated", "Topic successfully changed.");
-        } catch (IllegalArgumentException e) {
-            displayError("Topic not updated", e.getMessage());
-        }
-    }
-
 //    public void editTerm(String name, Topic topic) {
 //        try {
 //            termsService.saveTerm(name, topic, term);
@@ -121,15 +90,6 @@ public class TermsController extends Controller implements Serializable {
 //            displayError("Term not updated", e.getMessage());
 //        }
 //    }
-
-    public void deleteTopic() {
-        try {
-            termsService.deleteTopic(topic);
-            displayInfo("Topic deleted", "Topic successfully deleted.");
-        } catch (IllegalArgumentException e) {
-            displayError("Topic not deleted", e.getMessage());
-        }
-    }
 
 //    public void deleteTerm() {
 //        termsService.getTermsRepository().delete(term);
