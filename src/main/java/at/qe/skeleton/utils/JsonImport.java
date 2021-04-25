d6d364bad35a5971c35c9f8c1f029698c91517ec
Flaminia Anselmi <flaminia.anselmi@student.uibk.ac.at>
2021-04-25 10:22:14 +0200

    update json draft

--- src/main/java/at/qe/skeleton/utils/JsonImport.java
+++ src/main/java/at/qe/skeleton/utils/JsonImport.java
@@ -3,9 +3,13 @@ package at.qe.skeleton.utils;
 import org.apache.tomcat.util.json.JSONParser;
 import org.apache.tomcat.util.json.ParseException;
 import org.primefaces.shaded.json.JSONArray;
+import org.primefaces.shaded.json.JSONObject;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class JsonImport {
 
@@ -14,5 +18,45 @@ public class JsonImport {
         JSONParser jsonParser = new JSONParser(new FileReader(file));
 
         return (JSONArray) jsonParser.parse();
+    }
+
+    private Connection connect = null;
+    PreparedStatement preparedStatement = null;
+
+    public int insertJSONtoDB() throws Exception {
+        int status = 0;
+        try {
+            Class.forName("org.h2.Driver");
+            connect = DriverManager.getConnection("jdbc:h2:mem:skel", "sa", "");
+            PreparedStatement preparedStatement = connect.prepareStatement("insert into term values ( ?, ?, ? )");
+            //JSONParser parser = new JSONParser();
+            //Object obj = parser.parse(new FileReader("c.\\terms.json"));
+            //readJson("terms");
+            JSONObject jsonObject = new JSONObject(readJson("terms"));
+            JSONArray jsonArray = readJson("terms");
+
+            for(Object object : jsonArray) {
+                JSONObject record = (JSONObject) object;
+                String term_name = (String) record.get("Term_Name");    // from JSON tag
+                String topic_name = (String) record.get("Topic_Topic_Name");
+                preparedStatement.setString(1, term_name);  // to the Database table
+                preparedStatement.setString(2, topic_name);
+            }
+
+            status = preparedStatement.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connect != null) {
+                    connect.close();
+                }
+
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+        return status;
     }
 }
